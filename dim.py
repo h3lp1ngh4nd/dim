4b439d8f545a588b51ed5f2465ccdac8877ec5f4
h3lp1ngh4nd <99024638+h3lp1ngh4nd@users.noreply.github.com>
2022-02-04 13:41:37 +0100

    Update dim.py

--- dim.py
+++ dim.py
@@ -9,12 +9,20 @@ import sys
 import random
 
 
-LISK_EPOCH = 1464109200
+# Fixes/Updates by HelpingHand:
+# -converted code to be fully compatible with Python3
+# -API endpoints and json requests reworked to work with Liskv3
+# -numerous code changes in order to accomodate for Liskv3
+# -added feature to calculate actual delegate earnings (deduct voter share %)
 
+# TODO:
+# SHEER OVERKILL - Add a value calculation method to calculate actual token value at the time of forging (per block) and add this as an option
+# Value calculation option: Per actual block or per day.
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--network", help="use 'mainnet' or 'testnet'", choices=['mainnet', 'testnet', 'custom'])
-parser.add_argument("--address", help="specify delegate address")
+parser.add_argument("--username", help="specify delegate username")
+parser.add_argument("--share", help="specify voter's share")
 parser.add_argument("--start", help="specify start date (yyyy/mm/dd)")
 parser.add_argument("--end", help="specify end date (yyyy/mm/dd)")
 args = parser.parse_args()
@@ -23,17 +31,12 @@ args = parser.parse_args()
 def setup_network():
 	network_lookup = {	
 	"mainnet": [
-			"https://node01.lisk.io", 
-			"https://node02.lisk.io",
-			"https://node03.lisk.io",
-			"https://node04.lisk.io",
-			"https://node05.lisk.io",
-			"https://node06.lisk.io",
-			"https://node07.lisk.io",
-			"https://node08.lisk.io",
+			"https://service.lisk.com",
+			"https://mainnet-service.lisktools.eu",
 				],
 	"testnet": [
-			"https://testnet.lisk.io"
+			"https://testnet-service.lisk.com",
+			"https://testnet-service.lisktools.eu",
 				],
 	"custom": [
 			"http://127.0.0.1:7000"
@@ -42,44 +45,48 @@ def setup_network():
 
 	if args.network is None:
 		while True:
-			print "Choose a network (mainnet / testnet)"
-			answer = raw_input("> ")
-			answer_normalized = answer.lower().strip()	
-			print		
+			print("Choose a network (mainnet / testnet)")
+			answer = input("> ")
+			answer_normalized = answer.lower().strip()		
 			if answer_normalized in network_lookup:
 				node = network_lookup[answer_normalized][random.randint(0, len(network_lookup[answer_normalized]) - 1)]
+				network = answer_normalized
 				break
 
 	else:
 		node = network_lookup[args.network][random.randint(0, len(network_lookup[args.network]) - 1)]
 
-	print "Using node: %s" % node
-	node_status = get_json(node + "/api/node/status", True)
-	return node
+	print("Using service node: %s" % node)
+	node_status = get_json(node + "/network/status", True)
+	return node, network
 
 
 def setup_delegate():
-	if args.address is None:
-		while True:
-			print "Enter delegate address:"
-			answer = raw_input("> ")
-			answer_normalized = answer.strip()	
-			print
-			if re.match(r'\d+L', answer_normalized) != None:
-				address = answer_normalized
-				break
+	if args.username is None:
+		print("\nEnter delegate username:")
+		answer = input("> ")
+		username = answer.strip()                     
 	else:
-		address = args.address	
-
+		username = args.username.strip()
+	
+	if args.share is None:
+		print("\nEnter voter's share (in %):")
+		answer = input("> ")
+		share = int(answer.strip(' %'))
+		
+	else:
+		share = int(args.share.strip(' %'))
+		
 	try:
-		delegate = get_json(node + "/api/delegates?address=%s" % address)
-		publickey = delegate['data'][0]['account']['publicKey']
+		delegate = get_json(node + "/api/v2/accounts?username=%s" % username)
+		publickey = delegate['data'][0]['summary']['publicKey']
 	except:
 		sys.exit("Delegate not found.")
 
-	delegate_name = delegate['data'][0]['username']
-	print "Delegate found: %s\n" % delegate['data'][0]['username']
-	return address, publickey, delegate_name
+	address = delegate['data'][0]['summary']['address']
+	delegate_name = username
+	print("Delegate username %s found.\nAddress: %s\nSharing: %s percent\n" % (delegate_name, address, share))
+	return address, publickey, delegate_name, share
 
 
 def setup_filename(delegate_name):
@@ -96,12 +103,12 @@ def get_json(endpoint, is_alive_log=False):
 
 	if response.status_code == 200:
 		if is_alive_log:
-			print "Node is alive :)\n"
+			print("Node is alive :)\n")
 	elif response.status_code == 429:
-		print "Too many requests, exceeded rate limit."
+		print("Too many requests, exceeded rate limit.")
 		sys.exit()
 	elif response.status_code == 500:
-		print "Unexpected error"
+		print("Unexpected error")
 		sys.exit()
 
 	return response.json()
@@ -131,15 +138,15 @@ def get_total_values(blockdata):
 
 # Find the required amount of iterations to reach the nearest defined block (using timestamps)
 def seek_block(date, mode, iterations_total):
-	print "Seeking '%s' block..." % mode
+	print("Seeking '%s' block..." % mode)
 
 	iterations = 0
 	offset = 0
 	mult = 1
-	timestamp_input = int(create_timestamp(date)) - LISK_EPOCH
+	timestamp_input = int(create_timestamp(date))# - LISK_EPOCH - no longer needed for timestamp calculations
 	
-	for i in xrange(iterations_total):
-		json_data = get_json(node + "/api/blocks?limit=1&offset=%s&generatorPublicKey=%s" % (offset, publickey))
+	for i in range(iterations_total):
+		json_data = get_json(node + "/api/v2/blocks?limit=1&offset=%s&generatorPublicKey=%s" % (offset, publickey))
 		page_timestamp = json_data['data'][0]['timestamp']
 		timestamp_diff = page_timestamp - timestamp_input
 		
@@ -148,43 +155,45 @@ def seek_block(date, mode, iterations_total):
 		if mode == "end" and timestamp_diff < 0:
 			offset -= 100
 			break		
-		
+
+
 		if timestamp_diff > 200000:
-			mult = (timestamp_diff / 200000)
+			mult = int(timestamp_diff / 200000)
 		else:
 			mult = 1	
-		print "Diff %s, %s = %s" % (timestamp_input, page_timestamp, timestamp_diff)
-		print "Pages jumped: %s" % mult
+		print ("Diff %s, %s = %s" % (timestamp_input, page_timestamp, timestamp_diff))
+		print ("Pages jumped: %s" % mult)
 
-		offset += (100*mult)
-		iterations += (1*mult)
+		offset += int((100*mult))
+		iterations += int((1*mult))
 		# Prevent server ban by limiting API request rate
 		time.sleep(0.02)
 
 	return iterations, offset
 
 
-print '''
+print ('''
   ___    ___   __  __ 
  |   \  |_ _| |  \/  |
  | |) |  | |  | |\/| |
- |___/  |___| |_|  |_|  v1.0.0
-  '''          
-print "Delegate Income Monitor"
-print "Created by Lemii"
-print "____________________________________\n"
+ |___/  |___| |_|  |_|  v1.1.0
+  ''')        
+print ("Delegate Income Monitor")
+print ("Originally created by Lemii")
+print ("Updated & improved to function with Liskv3 by HelpingHand")
+print ("____________________________________\n")
 
 
 # Setup stuff
-node = setup_network()
-address, publickey, delegate_name = setup_delegate()
+node, network = setup_network()
+address, publickey, delegate_name, share = setup_delegate()
 filename = setup_filename(delegate_name)
-
+v3_genesis_epoch = 1629547210 #Saturday, August 21, 2021 2:00:10 PM CET - Block 16270293
 
 def main():
 
-	forging_stats = get_json(node + "/api/delegates/%s/forging_statistics" % address)
-	iterations_total = (int(forging_stats['data']['count']) / 100) + 1
+	forging_stats = get_json(node + "/api/v2/accounts?address=%s" % address)
+	iterations_total = int(forging_stats['data'][0]['dpos']['delegate']['producedBlocks'] / 100) + 1
 	blockstats =[]
 	current_day = 0
 	block_number = 0
@@ -192,15 +201,20 @@ def main():
 
 	# Interactive wizard for start date
 	if args.start is None:
+
+		print ("(OPTIONAL) Enter start date (yyyy/mm/dd):\n")
+		print ("Warning: If left empty, DIM will process *all* blocks ever")
+		print ("forged by the delegate. This can take a very long time.\n")
 		while True:
-			print "(OPTIONAL) Enter start date (yyyy/mm/dd):\n"
-			print "Warning: If left empty, DIM will process *all* blocks ever"
-			print "forged by the delegate. This can take a very long time."
-			answer = raw_input("> ")
-			print
+			answer = input("> ")
 			if re.match(r'\d{4}/\d{2}/\d{2}', answer) != None:
-				start = answer
-				break
+                                # Add a check to deny dates before genesis block of Liskv3
+				date_to_epoch = int(datetime.datetime.strptime(answer,"%Y/%m/%d").timestamp())
+				if date_to_epoch >= v3_genesis_epoch:   
+					start = answer
+					break
+				else:
+					print("ERROR: You have chosen a date before the creation of the Liskv3 Genesis Block silly. Try again!\n\n(OPTIONAL) Enter start date (yyyy/mm/dd):")
 			elif answer == "":
 				start = None
 				break
@@ -211,8 +225,8 @@ def main():
 	# Interactive wizard for end date
 	if args.end is None and start is not None:
 		while True:
-			print "(OPTIONAL) Enter end date (yyyy/mm/dd):"
-			answer = raw_input("> ")
+			print ("(OPTIONAL) Enter end date (yyyy/mm/dd):")
+			answer = input("> ")
 			if re.match(r'\d{4}/\d{2}/\d{2}', answer) != None:
 				end = answer
 				break
@@ -226,10 +240,10 @@ def main():
 	# Calculate necessary amount of iterations (number of API result pages to go through)
 	if start is not None:
 		iterations_start, offset_start = seek_block(start, "start", iterations_total)
-		print "Start block found.\n"
+		print ("Start block found.\n")
 		if end is not None:
 			iterations_end, offset_end = seek_block(end, "end", iterations_total)
-			print "End block found.\n"
+			print ("End block found.\n")
 			offset = offset_end
 			iterations_required = (iterations_start - iterations_end) + 1
 
@@ -242,17 +256,18 @@ def main():
 		offset = 0
 
 
-	print "Using offset: %s" % offset
-	print "Number of pages to process: %s (~%s blocks)\n" % (iterations_required, int(iterations_required) * 100)
+	#print ("Using offset: %s" % offset)
+	#print ("Number of pages to process: %s (~%s blocks)\n" % (iterations_required, int(iterations_required) * 100))
+	print ("Number of blocks to process: ~%s blocks\n" % (int(iterations_required) * 100))
 
 
 	# Get block results (limited to 100 per page)
-	for i in xrange(iterations_required):
-		block_list = get_json(node + "/api/blocks?limit=100&offset=%s&generatorPublicKey=%s" % (offset, publickey))
+	for i in range(iterations_required):
+		block_list = get_json(node + "/api/v2/blocks?limit=100&offset=%s&generatorPublicKey=%s" % (offset, publickey))
 		# Retrieve data from each individual block of the result page
 		for entry in block_list['data']:
 			ignore = 0
-			timestamp = LISK_EPOCH + int(entry['timestamp'])
+			timestamp = int(entry['timestamp']) # REMOVED OBSOLETE LISK_EPOCH +
 			if start != None and timestamp < create_timestamp(start):
 				ignore = 1
 				continue
@@ -271,12 +286,12 @@ def main():
 					lsk_btc_value = cc_api_data['LSK']['BTC']
 					lsk_usd_value = cc_api_data['LSK']['USD']
 					lsk_eur_value = cc_api_data['LSK']['EUR']
-					print "LSK value at %s: %.8f BTC, %.2f USD, %.2f EUR" % (date_time_simple, lsk_btc_value, lsk_usd_value, lsk_eur_value)
+					print ("LSK value at %s: %.8f BTC, %.2f USD, %.2f EUR" % (date_time_simple, lsk_btc_value, lsk_usd_value, lsk_eur_value))
 				except: 
 					lsk_btc_value = cc_api_data['LSK']['BTC']
 					lsk_usd_value = cc_api_data['LSK']['USD']
 					lsk_eur_value = cc_api_data['LSK']['EUR']
-					print "\nError fetching LSK value for %s" % date_time_simple
+					print ("\nError fetching LSK value for %s" % date_time_simple)
 				current_day = day_as_int
 			
 			if ignore != 1: 
@@ -285,22 +300,35 @@ def main():
 			else:
 				message_string = "Skipping block #%s, ID: %s" % (block_number, entry['id'])
 
-			sys.stdout.write('{0}\r'.format(message_string))
+			#sys.stdout.write('{0}\r'.format(message_string)) # If you like clutter, uncomment
 			block_number += 1
 			# Prevent server ban by limiting API request rate
-			time.sleep(0.02)
+			time.sleep(0.05)
 
 		offset += 100
 
 
-	print "\n\nSummary:"
-	#print "Address: %s (%s)" % (address, delegate_name)
-	#print "Start / End date: %s - %s" % (start, end)
+	print ("\n\nSummary:")
+	#print ("Address: %s (%s)" % (address, delegate_name))
+	#print ("Start / End date: %s - %s" % (start, end))
 	lsk, btc, usd, eur = get_total_values(blockstats)
-	print "Total LSK forged: %s " % lsk
-	print "Total BTC value: %s" % btc
-	print "Total USD value: %s" % usd
-	print "Total EUR value: %s" % eur
+
+	print("Total LSK forged: {:0.8f}".format(lsk))
+	print("Total BTC value: {:0.8f}".format(btc))
+	print("Total USD value: {:0.2f}".format(usd))
+	print("Total EUR value: {:0.2f}".format(eur))
+
+	print("\nShared with voters: %s percent" % share)
+	print("Voters LSK earned: {:0.8f}".format(lsk * (share / 100)))
+	print("Voters BTC earned: {:0.8f}".format(btc * (share / 100)))
+	print("Voters USD earned: {:0.2f}".format(usd * (share / 100)))
+	print("Voters EUR earned: {:0.2f}".format(eur * (share / 100)))
+	
+	print("\nDelegate share: %s percent" % (100-share))
+	print("Delegate LSK earned: {:0.8f}".format(lsk * ((100 - share) / 100)))
+	print("Delegate BTC earned: {:0.8f}".format(btc * ((100 - share) / 100)))
+	print("Delegate USD earned: {:0.2f}".format(usd * ((100 - share) / 100)))
+	print("Delegate EUR earned: {:0.2f}".format(eur * ((100 - share) / 100)))
 
 
 	# Write results to file
@@ -308,10 +336,10 @@ def main():
 		f.write("Datetime\tLSK amount\tBTC value\tUSD value\tEUR value\tBlock ID\n")
 		for row in blockstats:
 			f.write("%s\t%s\t%s\t%s\t%s\t%s\n" % (row[0], row[1], row[2], row[3], row[4], row[5]))
-		print "\nFile saved: %s" % filename
+		print ("\nFile saved: %s" % filename)
 
 
-	exit = raw_input("\nPress [ENTER] key to exit")
+	exit = input("\nPress [ENTER] key to exit")
 	sys.exit()
 
 
